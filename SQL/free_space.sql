77cadb427f46c50cec2603b5e9350b8c48cf35f8
maria <magsend@gmail.com>
2021-09-21 22:31:44 +0200

    added query result example

--- SQL/free_space.sql
+++ SQL/free_space.sql
@@ -1,4 +1,16 @@
 -- free space on disks
+-- RSE                               |FREE             |SITE                   |CLOUD|
+-- ----------------------------------+-----------------+-----------------------+-----+
+-- TAIWAN-LCG2_DATADISK              | 2596929044196508|Taiwan-LCG2            |TW   |
+-- GRIF-LAL_DATADISK                 |  214617114119111|GRIF-LAL               |FR   |
+-- UKI-SOUTHGRID-RALPP_DATADISK      |  393173778672980|UKI-SOUTHGRID-RALPP    |UK   |
+-- BNLHPC_DATADISK                   |   24713389635674|BNLHPC                 |US   |
+-- CA-WATERLOO-T2_DATADISK           |  213655125926702|CA-WATERLOO-T2         |CA   |
+-- UAM-LCG2_DATADISK                 |  106944959230034|UAM-LCG2               |ES   |
+-- AUSTRALIA-ATLAS_DATADISK          |  155365766269751|Australia-ATLAS        |CA   |
+-- INFN-GENOVA_DATADISK              |    2586517700608|INFN-GENOVA            |IT   |
+-- UKI-LT2-RHUL_DATADISK             |   82464391974032|UKI-LT2-RHUL           |UK   |
+
 WITH a AS (
 SELECT ATLAS_RUCIO.id2rse(RSE_ID) AS rse, free
 FROM ATLAS_RUCIO.rse_usage
