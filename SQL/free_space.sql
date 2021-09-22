4c97be855543ce0cef495cbc5fd548320176a650
maria <magsend@gmail.com>
2021-09-22 13:03:34 +0200

    added timestamp and samples file

--- SQL/free_space.sql
+++ SQL/free_space.sql
@@ -20,6 +20,6 @@ AND ATLAS_RUCIO.id2rse(RSE_ID) LIKE '%DATADISK'),
 b AS (SELECT ATLAS_RUCIO.id2rse(RSE_ID) AS rse, value AS site FROM ATLAS_RUCIO.RSE_ATTR_MAP WHERE KEY='site'),
 c AS (SELECT ATLAS_RUCIO.id2rse(RSE_ID) AS rse, value AS cloud FROM ATLAS_RUCIO.RSE_ATTR_MAP WHERE KEY='cloud'),
 d AS (SELECT b.rse, b.site, c.cloud FROM b, c WHERE b.rse = c.rse)
-SELECT a.*,
+SELECT sysdate AS timestamp, a.*,
 d.site,d.cloud
 FROM a,d WHERE a.rse = d.rse
