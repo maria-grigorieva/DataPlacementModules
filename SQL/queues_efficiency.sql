09b90169563794e5403662d4a5bdc95d689991bb
maria <magsend@gmail.com>
2021-09-22 21:40:19 +0200

    queues efficiency

---
+++ SQL/queues_efficiency.sql
@@ -0,0 +1,20 @@
+SELECT TRUNC(sysdate,'DAY') as datetime,
+       computingsite as queue,
+       NVL(ROUND(finished/(finished+failed),4),0) as efficiency
+       FROM
+       (SELECT * FROM
+(SELECT computingsite,
+       jobstatus,
+       count(*) as n_jobs
+FROM ATLAS_PANDA.JOBSARCHIVED4
+GROUP BY computingsite, jobstatus)
+PIVOT
+(
+   SUM(n_jobs)
+   for jobstatus in ('closed' as closed,
+       'cancelled' as cancelled,
+       'failed' as failed,
+       'finished' as finished
+))
+ORDER BY computingsite)
+ORDER BY efficiency DESC
