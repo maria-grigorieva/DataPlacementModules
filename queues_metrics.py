2f577fcd781a83ab3b448f2f7c5561fa1f96b49f
maria <magsend@gmail.com>
2021-09-25 23:05:22 +0200

    fixed: queues metrics
    Remove sites, clouds, rse

--- queues_metrics.py
+++ queues_metrics.py
@@ -56,12 +56,13 @@ def main(connection: str,
     """
 
     cx_Oracle.init_oracle_client(lib_dir=r"/usr/lib/oracle/19.3/client64/lib/")
+
     connection = cx_Oracle.connect(connection)
     cursor = connection.cursor()
     cursor.execute(occupancy, n_days = number_of_days)
     cursor.rowfactory = lambda *args: dict(zip([e[0] for e in cursor.description], args))
-    data = cursor.fetchall()
-    occupancy_df = pd.DataFrame(data)
+    occupancy_result = cursor.fetchall()
+    occupancy_df = pd.DataFrame(occupancy_result)
     cols = occupancy_df.columns
     occupancy_df.columns = [i.lower() for i in cols]
 
@@ -91,48 +92,14 @@ def main(connection: str,
     cursor = connection.cursor()
     cursor.execute(efficiency, n_days = number_of_days)
     cursor.rowfactory = lambda *args: dict(zip([e[0] for e in cursor.description], args))
-    data = cursor.fetchall()
-    efficiency_df = pd.DataFrame(data)
+    efficiency_result = cursor.fetchall()
+    efficiency_df = pd.DataFrame(efficiency_result)
     cols = efficiency_df.columns
     efficiency_df.columns = [i.lower() for i in cols]
 
     merged = pd.merge(occupancy_df, efficiency_df,
                       left_on='queue', right_on='queue')
-
-    df = pd.read_csv('data_samples/queue_site_disk.csv', index_col=[0])
-    #
-    # cric_base_url = 'https://atlas-cric.cern.ch/'
-    # url_queues = urllib.parse.urljoin(cric_base_url, 'api/atlas/pandaqueue/query/?json')
-    # cric_queues = requests.get(url_queues, cert=(ssl_cert, ssl_key), verify=tls_ca_certificate).json()
-
-    #
-    #
-    # queue_disk = []
-    # for queue in cric_queues:
-    #     if 'write_lan' in cric_queues[queue]['astorages']:
-    #         write_lan_rse = [i for i in cric_queues[queue]['astorages']['write_lan'] if 'DATADISK' in i]
-    #         #write_lan_anal_rse = [i for i in cric_queues[queue]['astorages']['write_lan_analysis'] if 'DATADISK' in i]
-    #         queue_disk.append({
-    #             'queue': queue,
-    #             'rse': list(set(write_lan_rse))
-    #         })
-    #
-    # queue_disk = pd.DataFrame(queue_disk)
-    # data = pd.DataFrame(data)
-    # data['site'] = data['COMPUTINGSITE'].apply(lambda x: cric_queues[x]['site'] if x in cric_queues else 'unknown')
-    # data['cloud'] = data['COMPUTINGSITE'].apply(lambda x: cric_queues[x]['cloud'] if x in cric_queues else 'unknown')
-    # data['tier_level'] = data['COMPUTINGSITE'].apply(lambda x: cric_queues[x]['tier_level'] if x in cric_queues else None)
-    # data.rename(columns={'COMPUTINGSITE':'queue'}, inplace=True)
-    # data.rename(columns={'SITE_OCCUPANCY_RATE':'queue_occupancy_rate'}, inplace=True)
-    # # remove TEST queues
-    # data = data[~data['queue'].str.contains('TEST')]
-    # cols = data.columns
-    # data.columns = [c.lower() for c in cols]
-
-    result = pd.merge(merged, df[['queue','site','cloud','rse']], left_on='queue', right_on='queue')
-    result = result.explode('rse')
-    result.to_csv('data_samples/queues_metrics.csv')
-
+    merged.to_csv('data_samples/queues_metrics.csv')
 
 if __name__ == "__main__":
     typer.run(main)
